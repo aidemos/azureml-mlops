910ef48671c268480294e2d81e43ca7ad6064aec
manukanwarpal <36815806+manukanwarpal@users.noreply.github.com>
2021-01-23 17:30:11 +0000

    added training pipeline

---
+++ ml_service/pipelines/mnist_build_training_pipeline.py
@@ -0,0 +1,153 @@
+import os
+import tarfile
+import urllib.request
+from azureml.core import Workspace
+from azureml.core.compute import AmlCompute, ComputeTarget
+from azureml.core.datastore import Datastore
+from azureml.core.dataset import Dataset
+from azureml.data.dataset_consumption_config import DatasetConsumptionConfig
+from azureml.pipeline.core import PipelineParameter
+from azureml.pipeline.core import Pipeline, PipelineData
+from azureml.core.model import Model
+from azureml.core import Environment
+from azureml.core.runconfig import CondaDependencies, DEFAULT_CPU_IMAGE
+from azureml.pipeline.core import PipelineParameter
+from azureml.pipeline.core import PublishedPipeline
+from azureml.pipeline.steps import ParallelRunStep, ParallelRunConfig, PythonScriptStep
+from azureml.core import Experiment
+from ml_service.util.env_variables import Env
+from ml_service.util.aml_helper import get_workspace
+from ml_service.util.aml_helper import get_compute
+from azureml.opendatasets import MNIST
+from azureml.core.runconfig import RunConfiguration
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core import Environment 
+
+def get_file_dataset(
+    ws: Workspace
+):
+
+    data_folder = os.path.join(os.getcwd(), 'data')
+    os.makedirs(data_folder, exist_ok=True)
+
+    mnist_file_dataset = MNIST.get_file_dataset()
+    mnist_file_dataset.download(data_folder, overwrite=True)
+
+    mnist_file_dataset = mnist_file_dataset.register(workspace=ws,
+                                                    name='mnist_opendataset',
+                                                    description='training and test dataset',
+                                                    create_new_version=True)
+    return mnist_file_dataset
+
+# def get_output_dir(
+#     ws: Workspace
+# ):
+#     def_data_store = ws.get_default_datastore()
+#     output_dir = PipelineData(name="inferences", datastore=def_data_store)
+#     return output_dir
+
+def register_model(
+    ws: Workspace
+):
+    # ### Download the Model
+    # create directory for model
+    model_dir = 'models'
+    if not os.path.isdir(model_dir):
+        os.mkdir(model_dir)
+
+    url="https://pipelinedata.blob.core.windows.net/mnist-model/mnist-tf.tar.gz"
+    response = urllib.request.urlretrieve(url, "model.tar.gz")
+    tar = tarfile.open("model.tar.gz", "r:gz")
+    tar.extractall(model_dir)
+
+    os.listdir(model_dir)
+
+    # ### Register the model with Workspace
+    model = Model.register(model_path="models/",
+                        model_name="mnist", # this is the name the model is registered as
+                        tags={'pretrained': "mnist"},
+                        description="Mnist trained tensorflow model",
+                        workspace=ws)
+    print("model registered")
+
+# def get_training_environment():
+#     training_conda_deps = CondaDependencies.create(pip_packages=['azureml-dataset-runtime[pandas,fuse]', 'azureml-defaults'], conda_packages = ['scikit-learn==0.22.1'])
+#     training_env = Environment(name="training_environment")
+#     training_env.python.conda_dependencies = training_conda_deps
+#     training_env.docker.enabled = True
+#     training_env.docker.base_image = DEFAULT_CPU_IMAGE
+#     return training_env
+
+def get_training_run_config(
+    compute_target: ComputeTarget,
+):
+
+
+    aml_run_config = RunConfiguration()
+    # `compute_target` as defined in "Azure Machine Learning compute" section above
+    aml_run_config.target = compute_target
+    aml_run_config.environment.python.user_managed_dependencies = False
+    
+    # Add some packages relied on by data prep step
+    aml_run_config.environment.python.conda_dependencies = CondaDependencies.create(pip_packages=['azureml-dataset-runtime[pandas,fuse]', 'azureml-defaults'], conda_packages = ['scikit-learn==0.22.1'])
+    return aml_run_config
+
+def get_training_pipeline(
+    ws: Workspace,
+    compute_target: ComputeTarget,
+    input_mnist_ds_consumption: Dataset,
+    training_run_config = RunConfiguration,
+):
+    scripts_folder = "mnist/training"
+    script_file = "train.py"
+
+    training_step = PythonScriptStep(
+    source_directory = scripts_folder,
+    script_name= script_file,
+    arguments=['--data-folder', input_mnist_ds_consumption.as_mount(), '--regularization', 0.5],
+    name="create-dataset",
+    compute_target=compute_target,
+    runconfig=training_run_config
+    )
+
+    pipeline = Pipeline(workspace=ws, steps=[training_step])
+
+    return pipeline
+
+def build_training_pipeline():
+    """
+    Main method that builds and publishes a scoring pipeline.
+    """
+    try:
+        #env = Env()
+        #ws = get_workspace(env)
+        ws = get_workspace()
+        compute_target = get_compute(ws)
+        input_dataset = get_file_dataset(ws)
+        training_run_config = get_training_run_config(compute_target)
+        pipeline = get_training_pipeline(ws,compute_target,input_dataset,training_run_config)
+        published_pipeline = pipeline.publish(
+            name="mnist_training",#env.scoring_pipeline_name,
+            description="MNIST Training Pipeline",
+        )
+        pipeline_id_string = published_pipeline.id     
+        print(pipeline_id_string)
+        return pipeline_id_string
+    except Exception as e:
+        print(e)
+        exit(1)
+
+def run_pipeline(
+    ws: Workspace,
+    pipelineid: str,
+    ):
+
+    experiment = Experiment(ws, 'mnist_training')
+    published_pipeline = PublishedPipeline.get(workspace=ws, id=pipelineid)
+    pipeline_run = experiment.submit(published_pipeline)
+    print("pipeline run submitted")
+
+if __name__ == "__main__":
+    ws = get_workspace()
+    pipeline_id = build_training_pipeline()
+    run_pipeline(ws,pipeline_id)
---
+++ ml_service/util/aml_helper.py
@@ -0,0 +1,64 @@
+import os
+import tarfile
+import urllib.request
+from azureml.core import Workspace
+from azureml.core.compute import AmlCompute, ComputeTarget
+from azureml.core.datastore import Datastore
+from azureml.core.dataset import Dataset
+from azureml.data.dataset_consumption_config import DatasetConsumptionConfig
+from azureml.pipeline.core import PipelineParameter
+from azureml.pipeline.core import Pipeline, PipelineData
+from azureml.core.model import Model
+from azureml.core import Environment
+from azureml.core.runconfig import CondaDependencies, DEFAULT_CPU_IMAGE
+from azureml.pipeline.core import PipelineParameter
+from azureml.pipeline.core import PublishedPipeline
+from azureml.pipeline.steps import ParallelRunStep, ParallelRunConfig
+from azureml.core import Experiment
+from ml_service.util.env_variables import Env
+
+def get_workspace(env: Env):
+        #Get Azure machine learning workspace
+        ws = Workspace.get(
+            name=env.workspace_name,
+            subscription_id=env.subscription_id,
+            resource_group=env.resource_group,
+        )
+
+        print("got workspace")
+
+        return ws
+
+def get_compute(
+    ws: Workspace
+):
+    # choose a name for your cluster
+    compute_name = os.environ.get("AML_COMPUTE_CLUSTER_NAME", "cpu-cluster")
+    compute_min_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MIN_NODES", 0)
+    compute_max_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MAX_NODES", 4)
+
+    # This example uses CPU VM. For using GPU VM, set SKU to STANDARD_NC6
+    vm_size = os.environ.get("AML_COMPUTE_CLUSTER_SKU", "STANDARD_D2_V2")
+
+
+    if compute_name in ws.compute_targets:
+        compute_target = ws.compute_targets[compute_name]
+        if compute_target and type(compute_target) is AmlCompute:
+            print('found compute target. just use it. ' + compute_name)
+            return compute_target
+    else:
+        print('creating a new compute target...')
+        provisioning_config = AmlCompute.provisioning_configuration(vm_size = vm_size,
+                                                                    min_nodes = compute_min_nodes, 
+                                                                    max_nodes = compute_max_nodes)
+
+        # create the cluster
+        compute_target = ComputeTarget.create(ws, compute_name, provisioning_config)
+        
+        # can poll for a minimum number of nodes and for a specific timeout. 
+        # if no min node count is provided it will use the scale settings for the cluster
+        compute_target.wait_for_completion(show_output=True, min_node_count=None, timeout_in_minutes=20)
+        
+        # For a more detailed view of current AmlCompute status, use get_status()
+        print(compute_target.get_status().serialize())
+        return compute_target
